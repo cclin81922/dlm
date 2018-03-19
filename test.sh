5d87c9f94d2caeec7288b0135dc9c5a5e2bc4752
Jim Lin <jimlintw922@gmail.com>
2018-03-19 20:21:57 +0800

    mod test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 set -eu
 set -o pipefail
 
+N={$N:-10}
+
 function _wait() {
     while [ $(screen -ls | grep process | wc -l) -gt 0 ]
     do
@@ -11,11 +13,16 @@ function _wait() {
 }
 
 function _launch() {
-    for i in `seq 1 100`; do screen -dmS process_$i go run main.go; done
+    for i in `seq 1 $N`; do screen -dmS process_$i go run main.go; done
 }
 
 # [ case 1 :: without-dlm ]
 function nodlm() {
+    echo
+    echo
+    echo
+    echo "case :: no dlm"
+
     echo "INFO |  $(date) | Launching"
     cp data/counter-init.txt data/counter.txt
     cd without-dlm
@@ -29,14 +36,19 @@ function nodlm() {
 
     echo "INFO | $(date) | Finished"
     echo
-    echo Expected 100
-    echo -n "Result: "
+    echo Expect $N
+    echo -n "Result "
     cat data/counter.txt
     echo
 }
 
 # [ case 2 :: with-dlm :: etcd ]
 function etcd() {
+    echo
+    echo
+    echo
+    echo "case :: with etcd dlm"
+
     docker run \
       --rm -d \
       -p 2379:2379 \
@@ -46,7 +58,7 @@ function etcd() {
       --data-dir=/etcd-data --name node1 \
       --initial-advertise-peer-urls http://127.0.0.1:2380 --listen-peer-urls http://0.0.0.0:2380 \
       --advertise-client-urls http://127.0.0.1:2379 --listen-client-urls http://0.0.0.0:2379 \
-      --initial-cluster node1=http://127.0.0.1:2380
+      --initial-cluster node1=http://127.0.0.1:2380 > /dev/null
     
     echo "INFO | $(date) | Launching"
     cp data/counter-init.txt data/counter.txt
@@ -61,20 +73,25 @@ function etcd() {
 
     echo "INFO | $(date) | Finished"
     echo
-    echo Expected 100
-    echo -n "Result: "
+    echo Expect $N
+    echo -n "Result "
     cat data/counter.txt
     echo
     
-    docker stop etcd-dev
+    docker stop etcd-dev > /dev/null
 }
 
 # [ case 3 :: with-dlm :: redis ]
 function redis() {
+    echo
+    echo
+    echo
+    echo "case :: with redis dlm"
+
     docker run \
       --rm -d \
       -p 6379:6379 \
-      --name redis-dev redis:4.0.8
+      --name redis-dev redis:4.0.8 > /dev/null
     
     echo "INFO | $(date) | Launching"
     cp data/counter-init.txt data/counter.txt
@@ -89,22 +106,27 @@ function redis() {
 
     echo "INFO | $(date) | Finished"
     echo
-    echo Expected 100
-    echo -n "Result: "
+    echo Expect $N
+    echo -n "Result "
     cat data/counter.txt
     echo
     
-    docker stop redis-dev
+    docker stop redis-dev > /dev/null
 }
 
 # case 4 :: with-dlm :: consul
 
 function consul() {
+    echo
+    echo
+    echo
+    echo "case :: with consul dlm"
+
     docker run \
       --rm -d \
       -p 8500:8500 \
       -e CONSUL_BIND_INTERFACE=eth0 \
-      --name consul-dev consul:1.0.6
+      --name consul-dev consul:1.0.6 > /dev/null
 
     echo "INFO | $(date) | Launching"
     cp data/counter-init.txt data/counter.txt
@@ -119,12 +141,12 @@ function consul() {
 
     echo "INFO | $(date) | Finished"
     echo
-    echo Expected 100
-    echo -n "Result: "
+    echo Expect $N
+    echo -n "Result "
     cat data/counter.txt
     echo
 
-    docker stop consul-dev
+    docker stop consul-dev > /dev/null
 }
 
 $1
